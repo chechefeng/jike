b85197ad06a1c1a298d6729d50d5b703af063d86
chechefeng <932481026@qq.com>
2018-11-01 21:02:46 +0800

    增加导航

--- src/app/containers/home/index.js
+++ src/app/containers/home/index.js
@@ -1,17 +1,20 @@
 import React, {Component} from 'react';
-import { Row, Col,DatePicker,Menu,Icon } from 'antd';
+import {Row, Col, Menu, Button} from 'antd';
 
 
 import "./css/index.css"
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup
+import Navs from "./nav"
 class Home extends Component {
 
     constructor(props) {
         super(props)
     }
+
     componentDidMount() {
     }
+
     componentWillMount() {
     }
 
@@ -19,36 +22,10 @@ class Home extends Component {
         return (
             <div >
 
-                <div className="header">
-                    <Row type="flex" justify="start">
-                        <Col  offset={4} style={{textAlign:"center"}} span={12}>Twilio Autopilot Demo Webinar | Register now</Col>
-                        <Col  offset={4} style={{width:"68px"}}>
-                            <Menu
-                                mode="horizontal"
-                            >
-                                <SubMenu title={"help"}>
-                                    <MenuItemGroup title="Item 1">
-                                        <Menu.Item key="setting:1">Option 1</Menu.Item>
-                                        <Menu.Item key="setting:2">Option 2</Menu.Item>
-                                    </MenuItemGroup>
-                                    <MenuItemGroup title="Item 2">
-                                        <Menu.Item key="setting:3">Option 3</Menu.Item>
-                                        <Menu.Item key="setting:4">Option 4</Menu.Item>
-                                    </MenuItemGroup>
-                                </SubMenu>
-                            </Menu>
-                        </Col>
-                        <Col   span={1}>login</Col>
-
-                    </Row>
-                    <DatePicker></DatePicker>
 
 
-
-
-
-
-                </div>
+                <hr/>
+                <Navs/>
             </div>
 
         )
