dc17da08422b628eb2a7641947c6b493a4701abc
chechefeng <932481026@qq.com>
2018-11-02 13:45:59 +0800

    增加其他板块儿

--- src/app/containers/home/index.js
+++ src/app/containers/home/index.js
@@ -5,6 +5,9 @@ import Part_2 from "./part2"
 import Part_3 from "./part3"
 import Part_4 from "./part4"
 import Part_5 from "./part5"
+import Part_6 from "./part6"
+import Part_7 from "./part7"
+import Part_8 from "./part8"
 
 import "./css/index.css"
 const SubMenu = Menu.SubMenu;
@@ -37,6 +40,9 @@ class Home extends Component {
                         <Part_3/>
                         <Part_4/>
                         <Part_5/>
+                        <Part_6/>
+                        <Part_7/>
+                        <Part_8/>
 
                     </div>
 
